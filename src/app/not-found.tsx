565497eb09270fea5ef0b71e5b044320a1abe29c
Vladislav Ometov <vladislavometov@MacBook.local>
2025-03-05 20:20:44 +0700

    Add 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import s from './not-found.module.scss'
+import Image from "next/image";
+import LinkWithWrapper from "@/app/components/Link/Link";
 
 export default function NotFound () {
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>404</h1>
-      <p>Такой страницы не существует</p>
-    </div>
+    <section className={s.page404}>
+      <div className="container">
+        <Image src={"/img/404.png"} width={640} height={265} alt={"KLЁN — architectural bureau"} layout="responsive"/>
+
+        <div className={s.text}>
+          <span>Страница не существует, была переименована или удалена</span>
+        </div>
+
+        <div className={s.linkList}>
+          <LinkWithWrapper link={"/"} dotReverce={false} isWrapper={false}
+                           name={"На главную"}/>
+          <LinkWithWrapper link={"/projects"} dotReverce={false} isWrapper={false}
+                           name={"Проекты"}/>
+        </div>
+      </div>
+    </section>
   );
 };

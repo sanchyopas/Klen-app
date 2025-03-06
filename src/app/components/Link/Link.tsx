818a1512046ab0dd090dda12345e51c93b278c6e
Vladislav Ometov <vladislavometov@MacBook.local>
2025-03-06 21:12:03 +0700

    Добавлен эффект переходов по ссылкам

--- src/app/components/Link/Link.tsx
+++ src/app/components/Link/Link.tsx
@@ -1,5 +1,10 @@
+'use client';
+
 import Link from "next/link";
 import s from "./link.module.scss"
+import {useEffect, useState} from "react";
+import {useRouter} from "next/navigation";
+
 
 type LinkProps = {
   dotReverce: boolean;
@@ -9,14 +14,40 @@ type LinkProps = {
   link?: string;
 }
 export default function LinkWithWrapper({className, dotReverce, isWrapper, name, link}: LinkProps) {
+
+  const router = useRouter();
+  const [isActive, setIsActive] = useState(false)
+  const [linkComponent, setLink] = useState('')
+
+  useEffect(() => {
+    if (isActive) {
+      const timeoutId = setTimeout(() => {
+        router.push(linkComponent);
+        setIsActive(false);
+      }, 750);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [isActive]);
+
+  const transitionEffect = (e: any) => {
+    e.preventDefault();
+    setLink(e.target.href);
+    setIsActive(true);
+  }
+
   return (
     <>
       {isWrapper ? (
         <div className={ className ? `${className} ${s.linkWrapper}` : s.linkWrapper}>
-          <Link href={link ?? "#"} className={`${s.link} ${dotReverce ? s.dotReverce : s.dot}`} >{name}</Link>
+          <Link onClick={transitionEffect} href={link ?? "#"} className={`${s.link} ${dotReverce ? s.dotReverce : s.dot}`} >{name}</Link>
+          <div className={ isActive? `${s.transataonEffect} ${s.active}` : s.transataonEffect}></div>
         </div>
       ) : (
-        <Link href={link ?? "#"} className={`${s.link} ${dotReverce ? s.dotReverce : s.dot}`} >{name}</Link>
+        <>
+          <Link onClick={transitionEffect} href={link ?? "#"} className={`${s.link} ${dotReverce ? s.dotReverce : s.dot}`} >{name}</Link>
+          <div className={ isActive? `${s.transataonEffect} ${s.active}` : s.transataonEffect}></div>
+        </>
       )}
     </>)
 }

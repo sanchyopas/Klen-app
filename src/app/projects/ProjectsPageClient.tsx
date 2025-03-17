0b574868c37ef3bbae1acf373801bfb44ddf66e3
Vladislav Ometov <vladislavometov@MacBook.local>
2025-03-18 04:59:27 +0700

    fix filter + animations

--- src/app/projects/ProjectsPageClient.tsx
+++ src/app/projects/ProjectsPageClient.tsx
@@ -28,38 +28,40 @@ type Props = {
 
 export default function ProjectsPageClient({ projects, filtersData }: Props) {
   const searchParams = useSearchParams();
-  const [filteredProjects, setFilteredProjects] = useState<Project[]>(projects);
+  const [filteredProjects, setFilteredProjects] = useState<Project[]>([]); // Начальное состояние — пустой массив
   const [filters, setFilters] = useState<{ type: string | null; year: string | null }>({
     type: null,
     year: null,
   });
 
+  // Применяем фильтры при изменении filters или projects
   useEffect(() => {
-    if (searchParams) {
-      const type = searchParams.get("type") ? decodeURIComponent(searchParams.get("type") as string) : null;
-      const year = searchParams.get("year") ? decodeURIComponent(searchParams.get("year") as string) : null;
+    const type = searchParams?.get("type") ? decodeURIComponent(searchParams.get("type") as string) : null;
+    const year = searchParams?.get("year") ? decodeURIComponent(searchParams.get("year") as string) : null;
 
-      setFilters({ type, year });
-    }
-  }, [searchParams?.toString()]);
+    setFilters({ type, year });
 
-  useEffect(() => {
     const filtered = projects.filter((project) => {
       const projectTypes = Array.isArray(project.getTypes) ? project.getTypes : [project.getTypes];
       const projectYears = Array.isArray(project.getYears) ? project.getYears : [project.getYears];
 
       return (
-        (!filters.type || projectTypes.includes(filters.type)) &&
-        (!filters.year || projectYears.includes(filters.year))
+        (!type || projectTypes.includes(type)) &&
+        (!year || projectYears.includes(year))
       );
     });
 
     setFilteredProjects(filtered);
-  }, [filters, projects]);
+  }, [searchParams?.toString(), projects]);
 
   return (
     <>
-      <Filter filtersData={filtersData} onFilterChange={setFilters} initialType={filters.type} initialYear={filters.year} />
+      <Filter
+        filtersData={filtersData}
+        onFilterChange={setFilters}
+        initialType={filters.type}
+        initialYear={filters.year}
+      />
       <Projects classes={s.projectsPage} projects={filteredProjects} />
     </>
   );

2591741434c2a562c58593bcc0947048c9a4bcfd
Vladislav Ometov <vladislavometov@MacBook.local>
2025-03-27 18:42:44 +0700

    fix dynamic blocks name

--- src/app/components/DynamicBlock/DynamicBlock.tsx
+++ src/app/components/DynamicBlock/DynamicBlock.tsx
@@ -16,35 +16,35 @@ import TextTwoColumn from "@/app/components/TextTwoColumn/TextTwoColumn";
 
 export const DynamicBlock = ({block}: any) => {
   switch (block.template) {
-    case "Текстовый блок - 2 колонки без заголовков":
+    case "Текст в 2 колонки":
       return <ProjectInfo description={block.text_left} details={block.text_right}/>
-    case "текст в 2 колонки":
+    case "Текст в 2 колонки по центру":
       return <TextTwoColumn text={block.text} />
-    case "Галлерея фото":
+    case "Фотогалерея":
       return <Slider slide_contian={true} slides={Array.isArray(block.image) ? block.image : []} is_boolet={true}/>
-    case "Текстовый блок - узкая колонка по центру":
+    case "Текст в 1 колонку по центру":
       return <SmallTextSection text={block.text}/>
-    case "Текст":
+    case "Текст по центру на 50% ширины":
       return <TextSection text={block.text}/>
-    case "Изображение на весь экран":
+    case "1 изображение (полноэкранное)":
       return <ParallaxImageBig image={block.image}/>
-    case "Изображение с отступом по краям":
+    case "1 изображение (на 50% ширины) по центру":
       return <SmallImage image={block.image}/>
-    case "Слева текст / Справа слайдер":
+    case "Слева - заголовок H3 + текст ниже | справа - слайдер изображений на 50% ширины (квадратные)":
       return <LeftTextRightSlider slides={block.slider} text={block.text} title={block.title}/>
     case "2 изображения в ряд (вертикальные) по центру":
       return <TwoColumnImage imageOne={block.image_1} imageTwo={block.image_2}/>
-    case "3 изображения без текста":
+    case "3 изображения в ряд (вертикальные)":
       return <ThreeColumnImage image_1={block.image_1} image_2={block.image_2} image_3={block.image_3}/>
-    case "Заголовок и текст слева + картинка справа":
+    case "Слева - заголовок H3 + текст ниже | справа - 1 изображение на 50% ширины":
       return <LeftTextRightImage image={block.image} text={block.text} title={block.title}/>
     case "2 изображения в ряд - на 2/3 ширины слева и 1/3 справа":
       return <TwoImageLeftWideNarrow image_left={block.image_left} image_right={block.image_right}  />
-    case "Заголовок h2 и текст слева + список справа":
+    case "Слева - заголовок H2 + текст ниже | справа - маркированный список":
       return <TitleTextLeftListRight text={block.text} title={block.title} as={'h2'} list={block.list}/>
-    case "Заголовок h3 и текст слева + список справа":
+    case "Слева - заголовок H3 + текст ниже | справа - маркированный список":
       return <TitleTextLeftListRight text={block.text} title={block.title} as={'h3'} list={block.list}/>
-    case "Заголовок h3 и текст слева + нумерованный список справа":
+    case "Слева - заголовок H3 + текст ниже | справа - нумерованный список":
       return <TitleTextLeftNumListRight text={block.text} title={block.title} list={block.list} />
     default:
       return
